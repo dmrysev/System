8d0c7c30957e1b211d5e0717f573d03463fa9597
dmrysev <dmrysev@protonmail.com>
2023-10-03 11:03:05 +0300

    Improvements to monitors recognition

--- Core/DesktopEnvironment.fs
+++ Core/DesktopEnvironment.fs
@@ -10,14 +10,21 @@ let smallScreenOutput = "eDP-1"
 let initTask() = async {
     while Util.Environment.XServer.isRunning() |> not do
         do! Util.Async.sleep (TimeSpan.FromSeconds 1)
-    Util.Process.run $"""
-        xrandr --output {bigScreenOutput} --auto --primary
-        xrandr --output {smallScreenOutput} --off
-        xinput --set-prop 'pointer:MX Ergo Mouse' 'libinput Accel Speed' -0.3 > /dev/null 2>&1
-    """
+    let isBigScreenConnected = (Util.Process.execute "xrandr") |> Util.String.contains $"{bigScreenOutput} connected"
+    if isBigScreenConnected then
+        Util.Process.run $"""
+            xrandr --output {bigScreenOutput} --auto --primary
+            xrandr --output {smallScreenOutput} --off
+            xinput --set-prop 'pointer:MX Ergo Mouse' 'libinput Accel Speed' -0.3 > /dev/null 2>&1
+        """
+    else
+        Util.Process.run $"""
+            xrandr --output {smallScreenOutput} --auto --primary
+        """
     while Util.Environment.WindowManagement.windowWithTitleExists "main_terminal" |> not do
         do! Util.Async.sleep (TimeSpan.FromSeconds 1)
-    Util.Process.run "wmctrl -a main_terminal -e 0,300,0,1400,800"
+    if isBigScreenConnected then Util.Process.run "wmctrl -a main_terminal -e 0,300,0,1400,800"
+    else Util.Process.run "wmctrl -a main_terminal -e 0,160,0,1100,700"
     do! Util.Async.sleep (TimeSpan.FromSeconds 5)
     Util.Process.run "wmctrl -a main_terminal"
 }
