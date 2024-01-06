1cf7eb48fb73bd98fec6ef095ff6879e8404c40f
dmrysev <dmrysev@protonmail.com>
2024-01-06 14:32:38 +0200

    CLI changes to WebBrowser

--- Core/WebBrowser.fs
+++ Core/WebBrowser.fs
@@ -7,16 +7,14 @@ open System
 
 [<Verb("web-browser", HelpText = "Start web browser.")>]
 type Options = { 
-    [<Value(0, MetaName = "Profile", Required = false, Default = "n")>] Profile: string }
+    [<Value(0, MetaName = "profile", Required = false, Default = "n")>] Profile: string
+    [<Option('t', "tracked", Required = false, Default = false)>] IsTracked: bool }
 
 let run (opts: Options) =
     let startOptions = { 
         Common.GuiApplication.StartOptions.Default with
             Maximized = true }
-    let startLibreWolf profile =
-        Common.GuiApplication.start startOptions $"librewolf -p {profile} > /dev/null 2>&1"
-    match opts.Profile with
-    | "n" -> startLibreWolf "n"
-    | "p" -> startLibreWolf "p"
-    | "g" -> Common.GuiApplication.start startOptions "firefox > /dev/null 2>&1"
-    | _ -> ()
+    if opts.IsTracked then
+        Common.GuiApplication.start startOptions $"firefox -p {opts.Profile} > /dev/null 2>&1"
+    else
+        Common.GuiApplication.start startOptions $"librewolf -p {opts.Profile} > /dev/null 2>&1"
